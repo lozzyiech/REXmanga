bb22f9a7c5070f856088958d04297963d200ebaa
ikuza <116513234+lozzyiech@users.noreply.github.com>
2025-05-02 12:31:32 +0300

    Update autoupdate.user.js

--- supscripts/autoupdate.user.js
+++ supscripts/autoupdate.user.js
@@ -7,56 +7,5 @@
 // @grant        none
 // ==/UserScript==
 
-/**
- * Настраивает автообновление скрипта при изменениях DOM и навигации
- * @param {Function} scriptFunc - Главная функция скрипта
- * @param {string} [containerSelector='body'] - Селектор контейнера для наблюдения
- */
-function setupAutoUpdate(scriptFunc, containerSelector = 'body') {
-    scriptFunc();
-    
-    // Наблюдатель за изменениями DOM
-    const observer = new MutationObserver((mutations) => {
-        const shouldReload = mutations.some(mutation => 
-            mutation.addedNodes.length > 0 || 
-            mutation.removedNodes.length > 0
-        );
-        
-        if (shouldReload) {
-            console.log('[AutoUpdate] DOM changed, reloading script');
-            scriptFunc();
-        }
-    });
-    
-    const container = document.querySelector(containerSelector);
-    if (container) {
-        observer.observe(container, {
-            childList: true,
-            subtree: true
-        });
-    }
-    
-    // Для SPA-приложений
-    const handleSPANavigation = () => {
-        console.log('[AutoUpdate] SPA navigation detected');
-        scriptFunc();
-    };
-    
-    window.addEventListener('popstate', handleSPANavigation);
-    window.addEventListener('pushState', handleSPANavigation);
-    window.addEventListener('replaceState', handleSPANavigation);
-    
-    // Перехват History API
-    const originalPushState = history.pushState;
-    const originalReplaceState = history.replaceState;
-    
-    history.pushState = function() {
-        originalPushState.apply(this, arguments);
-        window.dispatchEvent(new Event('pushState'));
-    };
-    
-    history.replaceState = function() {
-        originalReplaceState.apply(this, arguments);
-        window.dispatchEvent(new Event('replaceState'));
-    };
-}
+console.log('[AutoUpdate] SPA navigation detected');
+
